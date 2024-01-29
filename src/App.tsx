123614ef61047df08308de806296fb432c2a2337
Vodovva <valerievodovnik@gmail.com>
2024-01-29 14:12:22 -0600

    Add React-bootstrap and update Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import Navigation from './components/Navigation';
+import Container from 'react-bootstrap/Container';
 
 
 export default function App() {
@@ -11,12 +12,13 @@ export default function App() {
         {id: 2, title: 'React rules!'},
         {id: 3, title: 'How was your weekend?'}
     ]
-
   return (
-    <div>
-      <Navigation isLoggedIn={isLoggedIn} />
-      <h1>{isLoggedIn ? "Hello " + username : "Hello and Welcome"}</h1>
-      {posts.map( p =>  <h4 key={p.id}>{p.title}</h4> )}
-    </div>
-  );
+      <div>
+          <Navigation isLoggedIn={isLoggedIn} />
+          <Container>
+              <h1>{ isLoggedIn ? 'Hello ' + username : 'Hello and Welcome' }</h1>
+              { posts.map( p =>  <h4 key={p.id}>{p.title}</h4> ) }
+          </Container>
+      </div>
+  )
 }
